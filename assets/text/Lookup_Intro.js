b73c2c802a21163cc8485bd6dafdbf3d0cb086a4
Ada <39278228+Reithger@users.noreply.github.com>
2019-09-06 12:09:52 -0300

    Script for introductory sequence
    Tentative formatting; nested screen changes is the idea (each array entry is a set of swipeable screens, joined ideas).

--- assets/text/Lookup_Intro.js
+++ assets/text/Lookup_Intro.js
@@ -1,17 +1,76 @@
 export default {
     "default": {
-      "text" : "",
+      "text" : ["Canadian authorities ignoring hazards of costly refugee system"],
+      "format": ["headline"],
       "image" : require('../art/source_image_1.png'),
     },
 
     "screen" :
     [
-     {"text" : "",
+     {"text" : ["Canadian authorities ignoring hazards of costly refugee system"],
+      "format" : ["headline"],
       "image" : require('../art/source_image_1.png'),
       },
-     {"text" : "",
+     {"text" : ["Worried parents in Halifax are reporting outbreaks of violence at a local "+
+                "elementary school with Syrian refugee students threatening their fellow classmates.",
+               "One distraught mother told the Metro Morning New that a refugee student posted"+
+                "an Arabic message on Facebook threatening to hurt his classmates.",
+               "\"The people running the school don't seem to want to face the truth,\" said the woman"+
+                "who asked not to be identified for fear of reprisals."],
+      "format" : ["subhead", "body", "body"],
       "image" : require('../art/source_image_1.png'),
      },
+     {"text" : ["The incident has led experts to warn that Canada’s open-door refugee policies are not "+
+		"only costing taxpayers up to $50 billion every year, they're also sowing a \"culture of "+
+		"violence\" in communities across the country.",
+		"\"I know it seems unbelievable, but Canadian politicians are doling out billions for a "+
+		"failed refugee system that is causing direct harm to the citizens who are expected to +"
+		"foot the bill,\"  a former Canadian diplomat said during a Montreal public policy forum on"+
+		" the links between terrorism and immigration.],
+      "format" : ["subhead", "body"],
+      "image" : require("../art/stand_in.png"),
+     },
+     {"text" : ["Canada has recently announced plans to increase immigration levels to at least one million"+
+		" over the next three years with a special focus on African countries.",
+		"The plan to increase immigration levels worries experts who warn of inevitable clashes"+
+		" between traditional Canadian values and the cultural practices of foreign newcomers.",
+		"Hundreds of travellers cancelled their room bookings at a posh Toronto hotel, for example,"+
+		" after reports that refugees were slaughtering animals in its public bathrooms. Nearly"+
+		" 200 refugees live in the hotel at government expense as they await permanent housing."],
+      "format" : ["subhead", "body", "body"],
+      "image" : require("../art/stand_in.png"),
+     },
+     {"text" : ["Firefighters were called to the scene when a fire was set at the hotel in an apparent"+
+		" arson attack.",
+		"A woman has been charged for placing a burning gas canister in the hallway on the third"+
+		" floor. The fire set off the smoke alarms and alerted a hotel employee, who was able to"+
+		" kick the gas canister into a stairwell before the fire could spread."],
+      "format" : ["subhead", "body"],
+      "image" : require("../art/stand_in.png"),
+     },
+     {"text" : ["In another incident, a photo emerged showing Muslims in Canada celebrating after ISIS"+
+		" claimed responsibility for the deadly mass shooting in Toronto last summer in which"+
+		" multiple people were killed and injured."],
+      "format" : ["headline"],
+      "image" : require("../art/stand_in.png"),
+     },
+     {"text" : ["At a recent election rally, the newly formed Populist Party leader promised to accept"+
+		" fewer refugees, pledged to repeal the Multiculturalism Act, and committed to reject "+
+		"immigrants that do not share Canadian values.",
+		"The announcement garnered loud applause and cheers from the crowd who expressed"+
+		" resentment over the generous government subsidies available to refugees compared to"+
+		" meagre allowances for old-age pensioners and disabled Canadians."],
+      "format" : ["subhead", "body"],
+      "image" : require("../art/stand_in.png"),
+     },
+     {"text" : ["\"My mother Brenda raised three kids and worked as a cook at a local restaurant for 35"+
+		" years,\" says Suzanne Duncan of Chilliwack, B.C.\"",
+		"\"Now that she's bedridden because of a stroke, her disability pension allows her less"+
+		" than $5 a day for food while refugees in Vancouver hotels are getting $60,\" she adds.",
+		"\"They say refugees deserve our sympathy and compassion, but what about my mother? What"+
+		" does she deserve?\""],
+      "format" : ["body", "body", "body"],
+      "image" : require("../art/stand_in.png"),
+     },
   ]
-
-  }
+}
