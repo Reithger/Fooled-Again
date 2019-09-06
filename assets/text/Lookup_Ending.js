d2bd1823b4ad980bdd7e4e277462cf00709aee63
Ada <39278228+Reithger@users.noreply.github.com>
2019-09-06 12:17:43 -0300

    Script for end of game sequence

---
+++ assets/text/Lookup_Ending.js
@@ -0,0 +1,26 @@
+export default {
+    "default": {
+      "text" : ["Canadian authorities ignoring hazards of costly refugee system"],
+      "image" : require('../art/source_image_1.png'),
+    },
+
+    "present" : {
+      "text" : "Hey friends, I saw that people were getting worked up about this"+
+		" news story circulating on social media so I talked to "+
+		"a few folks and did some fact checking. Turns out that not" +
+		" everything in the story is true. See below for which parts "+
+		"of the story can be verified. Don’t get fooled by fake news!",
+      "image" : require("../art/stand_in.png"),
+    },
+    "victory" : {
+      "text" : "Anti-immigration protests rocked the city today "+
+		"as a result of a fake news story that falsely "+
+		"claimed refugees were causing violence.",
+      "image" : "require("../art/stand_in.png"),
+    },
+    "failure" : {
+      "text" : "Fact checkers saved the city from potential riots today when "+
+		"they exposed a viral anti-immigrant story as Fake News.",
+      "image" : require("../art/stand_in.png"),
+    },
+}
